899c7f0bb4a44abc4e8adeed9e41f70a7b354b06
MrBlu3Sky57 <tudor.avram@icloud.com>
2023-02-09 20:33:10 -0500

    add for loop

--- loop.c
+++ loop.c
@@ -3,8 +3,14 @@
 int main(void)
 {
     int i = 0;
-    while (i < 3) {
-        printf("Something");
+    while (i < 3) 
+    {
+        printf("Something\n");
         i++;
+    }
+
+    for (int j = 0; j < 3; j++) 
+    {
+        printf("Something\n");
     }
 }
